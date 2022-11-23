d4d9fc9e76e5e162dfc9b9e61035c0b5c403d71c
Roxanne Devillers <roxannedevillers@gmail.com>
2022-11-23 16:49:10 -0500

    ongoing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'Nettoyons la DB...'
+
+Flat.destroy_all
+
+puts 'Créons des flats..'
+
+4.times do
+  flat = Flat.create!(
+    name: Faker::TvShows::BrooklynNineNine.character,
+    address: Faker::Address.street_address,
+    price_per_night: rand(100..400),
+    number_of_guests: rand(1..6)
+  )
+  puts "Flat #{flat.id} a été créé"
+end
+
+puts 'Tout fini!'
